f63caa3e06d12a9fcccc8f45f0b923981221e547
Amr El-Bakry <amrrbakry17@gmail.com>
2017-12-20 23:20:57 +0200

    Get project dependencies from bundler definition

--- lib/rsyslibs.rb
+++ lib/rsyslibs.rb
@@ -1,5 +1,19 @@
 require 'rsyslibs/version'
+require 'bundler'
 
 module Rsyslibs
-  # Your code goes here...
+  class Dependencies
+    def self.system_dependencies
+      # get project_dependencies
+      # get os info
+      # make an api call with dependencies and os info
+      # parse the response and return it
+    end
+
+    private
+
+    def project_dependencies
+      Bundler.definition.dependencies.each(&:name)
+    end
+  end
 end
