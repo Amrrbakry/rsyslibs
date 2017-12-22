37ef636f92fb113960cf28ac6db2eb60f7ea6dce
Amr El-Bakry <amrrbakry17@gmail.com>
2017-12-22 02:44:42 +0200

    Change attribute name

--- lib/rsyslibs/operating_system_info.rb
+++ lib/rsyslibs/operating_system_info.rb
@@ -6,7 +6,7 @@ module Rsyslibs
     def self.os_info
       os_report = YAML.safe_load(OS.report)
       {
-        os_name: os_report['target_os'],
+        os: os_report['target_os'],
         os_vendor: os_report['target_vendor'],
         os_bits: OS.bits,
         ruby_platform: os_report['RUBY_PLATFORM']
